bb95bfe769f525adbfd8d4105345a28a9be42464
byte <50130120+huckleton@users.noreply.github.com>
2024-02-24 16:07:29 -0500

    pcard links

--- _config/profileCard.js
+++ _config/profileCard.js
@@ -1,10 +1,17 @@
 module.exports = function (eleventyConfig) {
-	function profileCard (content, iconPath) {
+	function profileCard (content, iconPath, links) {
+		let linksList = '<ul class="pcard-links">';
+		for (const key in links) {
+			linksList += `<li><a href="${links[key]}">${key}</a></li>`;
+		}
+		linksList += '</ul>';
+		console.log(linksList);
 
 		return `
 			<article class="profile-card">
 			<aside class="pcard-left">
 				<img src="/assets/img/${iconPath || "bust.svg"}" class="pcard-icon" alt="Icon for {{pageTitle}}">
+				${linksList}
 			</aside>
 			<main class="pcard-right">
 				${content}
